63274a753da91c32a0cde9be265773db532531e6
Nicholas <n.paz4444@gmail.com>
2021-12-30 20:43:21 -0500

    algorithm

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+	public static void main(String[] args){
+
+	}
+}
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+// int lastWordLength = 0;
+// int charAccumulator = 0;
+//
+// for loop
+// for(int i=0; i<s.length; i++){
+// if (s.charAt(i) ==" ") {
+//  update the lastWordLength variable
+//  lastWordLength == charAccumulator;
+//  reset the charAccumulator for next word
+//  charAccumulator = 0
+// }
+//  if we encounter a char (letter) increment the charAccumulator
+// else{
+// 	charAccumulator++;
+// }
+//
+// return lastWordLength;
