f1c7f97e34beef690834efd09ca3787a9ae3a28f
Nicholas <n.paz4444@gmail.com>
2021-12-30 21:01:04 -0500

    new algorithm starting at the end of string

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,29 @@
 public class Main {
-	public static void main(String[] args){
+	public static void main(String[] args) {
+		// Input: s = "   fly me   to   the moon  "
+		// Output: 4
+		// Explanation: The last word is "moon" with length 4.
 
+		String s = "   fly me   to   the moon  ";
+		System.out.println("Example 2 should return 4: " + lengthOfLastWord(s));
+	}
+
+	public static int lengthOfLastWord(String s) {
+		int lastWordLength = 0;
+
+		//to get the last word lengt start your for loop at the end
+		for (int i = s.length() - 1; i >= 0; i--) {
+			if (s.charAt(i) == ' ' && lastWordLength > 0) {
+				return lastWordLength;
+			} else if (s.charAt(i) != ' ') {
+				lastWordLength++;
+			}
+		}
+		return 0;
 	}
 }
 
-// Input: s = "   fly me   to   the moon  "
-// Output: 4
-// Explanation: The last word is "moon" with length 4.
 
-// int lastWordLength = 0;
-// int charAccumulator = 0;
-//
-// for loop
-// for(int i=0; i<s.length; i++){
-// if (s.charAt(i) ==" ") {
-//  update the lastWordLength variable
-//  lastWordLength == charAccumulator;
-//  reset the charAccumulator for next word
-//  charAccumulator = 0
-// }
-//  if we encounter a char (letter) increment the charAccumulator
-// else{
-// 	charAccumulator++;
-// }
-//
-// return lastWordLength;
+
+
+
